82815a662a37632ca264c9c6e16270475e60e7e0
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-10-11 03:45:08 +0600

    ok

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             <h1 className='font-bold text-2xl text-center'>Welcome To <span className='text-orange-500 font-bold text-2xl'>Q-Quiz</span></h1>
             <p className='1xl font-semibold text-center'><span className='text-orange-500 font-bold'>Q-Quiz</span> is a programming quiz website.In this site, people can practice the quiz of different programming languages.</p>
             <p className='text-center'>{topics.data.length}</p>
-            <div className='grid grid-cols-3 gap-5'>
+            <div className='mx-5 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2'>
             {
                 topics.data.map(topic => <Topics 
                     key={topic.id}
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Topics = ({topic}) => {
 const {name,logo,total} = topic;
     
     return (
         <div className='mt-10'>
-            <div className='border m-4'>
+            <div className='border m-6 w-10/12 bg-gray-100'>
                 <div>
                 <img className='w-1/2 mx-auto my-0 mt-2' src={logo} alt="" />
                 </div>
           
-            <div className='flex p-5 text-center'>
-            <h1 className=''>{name}</h1>
-            <p className='ml-28'>Quiz:{total}</p>
-            <button className='bg-sky-500 p-3 mx-auto'>Start Practice</button>
+            <div className='block p-5 text-center justify-evenly lg:flex'>
+            <h1 className='font-bold text-2xl'>{name}</h1>
+            <p className='font-semibold text-2xl'>Quiz:{total}</p>
+            <button className='bg-sky-500 p-2 ml-5'>Start Practice<FontAwesomeIcon className='ml-2' icon={faArrowRight}></FontAwesomeIcon></button>
             </div>
             
             </div>
